dd86065e022bc7f78615d00686d6fdd847ea3d7f
hope-bit <58602655+hope-bit@users.noreply.github.com>
2024-11-02 17:45:08 -0400

    Initialized scripts and dataset

--- Galaxy_Dataset.py
+++ Galaxy_Dataset.py
@@ -1,12 +1,47 @@
+import pandas as pd
+
 from galaxy_mnist import GalaxyMNIST
 
-# 64 pixel images
-dataset = GalaxyMNIST(
-    root='/some/download/folder',
-    download=True,
-    train=True  # by default, or set False for test set
-)
 
-images, labels = dataset.data, dataset.targets
+if __name__ == '__main__':
 
-(custom_train_images, custom_train_labels), (custom_test_images, custom_test_labels) = dataset.load_custom_data(test_size=0.8, stratify=True)
+    dataset = GalaxyMNIST(
+        root = 'C:/Users/HopeW/Downloads/MEM679_Project/SpaceScope/galaxy_mnist',
+        download=True,
+        train=True,  # by default, or False for canonical test set
+    )
+
+    """There is also a 'highrez' (aka 224x224 pixel) version, with the same galaxies"""
+    # dataset = GalaxyMNISTHighrez(
+    #     root='/home/walml/repos/galaxy_mnist/download_root_hirez',
+    #     download=True,
+    #     train=True,
+    # )
+
+    # this is always the canonical 80/20 train/test split
+    images = dataset.data
+    labels = dataset.targets
+
+    print(images.shape, images.dtype)
+    print(labels.shape, labels.dtype)
+
+
+    # however, you can set your own split size and stratification if you like
+    (custom_train_images, custom_train_labels), (custom_test_images, custom_test_labels) = dataset.load_custom_data(test_size=0.8, stratify=True) 
+    print(custom_train_images.shape, custom_test_images.shape)
+    print(pd.value_counts(custom_train_labels.numpy()), pd.value_counts(custom_test_labels.numpy()))
+
+
+    # The values of dataset.data, dataset.targets, and hence __getitem__,
+    # will be from whichever loading method you last called: either __init__ or load_custom_data
+    train_image, train_label = dataset[0]
+    print(train_image)
+    print(train_label)
+
+    dataset.train = False
+    # need to call again to update self.data, self.targets
+    _= dataset.load_custom_data(test_size=0.8, stratify=True)
+    # now __getitem__ returns test values
+    test_image, test_label = dataset[0]
+    print(test_image)
+    print(test_label)
